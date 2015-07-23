7dd748e22c74f66af7f3ba4479e22290afe05a63
ChristopherSalam <christopher.salam@gmail.com>
2015-07-23 02:12:49 -0700

    Made some progess on this chat app... numerous small errors, but now have some recurring cross origin request ping to talk to TAs about.

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,7 +1,7 @@
 /* Import node's http module: */
-var requestHandler = require("./request-handler.js");
-var http = require("http");
-
+var messages = require('./request-handler.js');
+var http = require('http');
+var urlParser = require('url');
 
 // Every server needs to listen on a port with a unique number. The
 // standard port for HTTP servers is port 80, but that port is
@@ -15,7 +15,10 @@ var port = 3000;
 // special address that always refers to localhost.
 var ip = "127.0.0.1";
 
-
+var router = {
+  '/classes/chatterbox/': require('./request-handler.js')
+  // ...
+};
 
 // We use node's http module to create a server.
 //
@@ -23,20 +26,19 @@ var ip = "127.0.0.1";
 // incoming requests.
 //
 // After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(requestHandler);
-console.log("Listening on http://" + ip + ":" + port);
-server.listen(port, ip);
+var server = http.createServer(function (req, res) {
 
-// To start this server, r
-//
-//   node basic-server.js
-//
-// on the command line.
-//
-// To connect to the server, load http://127.0.0.1:3000 in your web
-// browser.
-//
-// server.listen() will continue running as long as there is the
-// possibility of serving more requests. To stop your server, hit
-// Ctrl-C on the command line.
+    var parts = urlParser.parse(req.url);
+    console.log(parts);
 
+    console.log("Serving request type " + req.method + " for url " + req.url);
+    var route = router[url.parse(req.url).pathname];
+    if (route) {
+      route(req, res);
+    } else {
+      messages.send(res, "", 404);
+    }
+
+    console.log("Listening on http://" + ip + ":" + port);
+    server.listen(port, ip);
+  });
--- server/request-handler.js
+++ server/request-handler.js
@@ -1,105 +1,62 @@
-var qs = require('querystring'); /// for query.parse function
-
-/*************************************************************
-
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
-
-module.exports = function(request, response) {
-  // Request and Response come from node's http module.
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-  // Do some basic logging.
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-
-  // The outgoing status.
-  var responseData = null;
-  var messages = [];
-  // responseData = {"results":[]};
-  var statusCode = null;
-  // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  var fullBody = '';
-  var hasRequestMethod = false;
-
-  function send(response, data, statusCode){
-    statusCode = statusCode || 200
-    response.writeHead(statusCode, headers);
-    response.end(JSON.stringify(data));
-  }
-  function collectData(response, callback) {
-    var fullBody = '';
-    request.on('data', function(chunk){
-      fullBody += chunk;
-    }); 
-    request.on('end',  function(){
-      callback(JSON.parse(fullBody));
-    });
-  }
-  if (request.method === "GET") {
-    // hasRequestMethod = true;
-    var messagesObject = {
-      results: messages
-    };
-    send(response, messagesObject);
-  } 
-  else if (request.method === "POST") {  
-    // hasRequestMethod = true;
-    collectData(response, function(message){
-      console.log('message before', messages);
-      messages.push(message);
-      send(response, 'confirm', 201)
-    });
-    console.log('messages after', messages);
-  }
-  // if (!hasRequestMethod) {
-  //   statusCode = 404;
-  //   response.writeHead(statusCode, headers);
-  //   response.end(responseDataString);
-  // }
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-};
-
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
+var headers = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
+  "access-control-allow-headers": 'Content-Type, Authorization, Content-Length, X-Requested-With',
   "access-control-max-age": 10, // Seconds.
   "Content-Type": "application/json"
 };
 
+var objectId = 1;
+
+var collectData = function (response, callback) {
+    var fullBody = '';
+    request.on('data', function (chunk) {
+      fullBody += chunk;
+    });
+    request.on('end',  function () {
+      callback(JSON.parse(fullBody));
+    });
+  }
+
+exports.send = function(response, data, statusCode) {
+  statusCode = statusCode || 200
+  response.writeHead(statusCode, headers);
+  response.end(JSON.stringify(data));
+}
+
+var messages = [
+ {
+  text: 'sup',
+  username: 'fred',
+  objectId: objectId
+ }
+]; //results: messages
+
+module.exports = function(request, response) {
+
+  var responseData = null;
+  var statusCode = null;
+  var fullBody = '';
+  var hasRequestMethod = false;
+
+  if (request.method === "GET") {
+
+    exports.send(response, {results: messages});
+
+  } else if (request.method === "POST") {
+
+    collectData(response, function(message) {
+      message.objectId = ++objectId;
+      messages.push(message);
+      exports.send(response, {objectId: objectId}, 201)
+    });
+
+    console.log('messages after', messages);
+
+  } else if (request.method === "OPTIONS") {
+      exports.send(response, null);
+  } else {
+    exports.send(response, '', 404);
+  }
+
+};
