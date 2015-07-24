102597c793d784a0045189407f6a6225090d4c75
ChristopherSalam <christopher.salam@gmail.com>
2015-07-23 23:41:25 -0700

    worked on this some with group

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,44 +1,55 @@
 /* Import node's http module: */
-var messages = require('./request-handler.js');
-var http = require('http');
-var urlParser = require('url');
-
-// Every server needs to listen on a port with a unique number. The
-// standard port for HTTP servers is port 80, but that port is
-// normally already claimed by another server and/or not accessible
-// so we'll use a standard testing port like 3000, other common development
-// ports are 8080 and 1337.
+var http = require("http");
+var hand = require("./request-handler.js");
 var port = 3000;
-
-// For now, since you're running this server on your local machine,
-// we'll have it listen on the IP address 127.0.0.1, which is a
-// special address that always refers to localhost.
 var ip = "127.0.0.1";
+var server = http.createServer(hand.requestHandler);
+console.log("Listening on http://" + ip + ":" + port);
+server.listen(port, ip);
 
-var router = {
-  '/classes/chatterbox/': require('./request-handler.js')
-  // ...
-};
 
-// We use node's http module to create a server.
-//
-// The function we pass to http.createServer will be used to handle all
-// incoming requests.
-//
-// After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(function (req, res) {
 
-    var parts = urlParser.parse(req.url);
-    console.log(parts);
+// /* Import node's http module: */
+// var messages = require('./request-handler.js');
+// var http = require('http');
+// var urlParser = require('url');
 
-    console.log("Serving request type " + req.method + " for url " + req.url);
-    var route = router[url.parse(req.url).pathname];
-    if (route) {
-      route(req, res);
-    } else {
-      messages.send(res, "", 404);
-    }
+// // Every server needs to listen on a port with a unique number. The
+// // standard port for HTTP servers is port 80, but that port is
+// // normally already claimed by another server and/or not accessible
+// // so we'll use a standard testing port like 3000, other common development
+// // ports are 8080 and 1337.
+// var port = 3000;
 
-    console.log("Listening on http://" + ip + ":" + port);
-    server.listen(port, ip);
-  });
+// // For now, since you're running this server on your local machine,
+// // we'll have it listen on the IP address 127.0.0.1, which is a
+// // special address that always refers to localhost.
+// var ip = "127.0.0.1";
+
+// var router = {
+//   '/classes/chatterbox/': require('./request-handler.js')
+//   // ...
+// };
+
+// // We use node's http module to create a server.
+// //
+// // The function we pass to http.createServer will be used to handle all
+// // incoming requests.
+// //
+// // After creating the server, we will tell it to listen on the given port and IP. */
+// var server = http.createServer(function (req, res) {
+
+//     var parts = urlParser.parse(req.url);
+//     console.log(parts);
+
+//     console.log("Serving request type " + req.method + " for url " + req.url);
+//     var route = router[url.parse(req.url).pathname];
+//     if (route) {
+//       route(req, res);
+//     } else {
+//       messages.send(res, "", 404);
+//     }
+
+//     console.log("Listening on http://" + ip + ":" + port);
+//     server.listen(port, ip);
+//   });
--- server/request-handler.js
+++ server/request-handler.js
@@ -1,62 +1,117 @@
-var headers = {
+
+var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": 'Content-Type, Authorization, Content-Length, X-Requested-With',
-  "access-control-max-age": 10, // Seconds.
-  "Content-Type": "application/json"
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10 // Seconds.
 };
 
-var objectId = 1;
+var messages = [];
 
-var collectData = function (response, callback) {
-    var fullBody = '';
-    request.on('data', function (chunk) {
-      fullBody += chunk;
-    });
-    request.on('end',  function () {
-      callback(JSON.parse(fullBody));
-    });
-  }
+var requestHandler = function(request, response) {
 
-exports.send = function(response, data, statusCode) {
-  statusCode = statusCode || 200
-  response.writeHead(statusCode, headers);
-  response.end(JSON.stringify(data));
-}
 
-var messages = [
- {
-  text: 'sup',
-  username: 'fred',
-  objectId: objectId
- }
-]; //results: messages
+  console.log("Serving request type " + request.method + " for url " + request.url);
+  var statusCode = 200;
 
-module.exports = function(request, response) {
+  // See the note below about CORS headers.
+  var headers = defaultCorsHeaders;
+  var responseData;
 
-  var responseData = null;
-  var statusCode = null;
-  var fullBody = '';
-  var hasRequestMethod = false;
+  headers['Content-Type'] = "text/plain";
 
   if (request.method === "GET") {
-
-    exports.send(response, {results: messages});
-
+   if (request.url === "/classes/messages"){
+      sendEnd(response, 200, JSON.stringify({results:messages}), headers);
+    } else if (request.url === '/classes/room1') {
+      sendEnd(response, 200, JSON.stringify({results:messages}), headers);
+    } else {
+      sendEnd(response, 404, "File 404, File not found", headers)
+    }
   } else if (request.method === "POST") {
-
-    collectData(response, function(message) {
-      message.objectId = ++objectId;
-      messages.push(message);
-      exports.send(response, {objectId: objectId}, 201)
-    });
-
-    console.log('messages after', messages);
-
-  } else if (request.method === "OPTIONS") {
-      exports.send(response, null);
-  } else {
-    exports.send(response, '', 404);
+    if (request.url === "/classes/messages") {
+      request.on('data', function(chunks) {
+        messages.push(JSON.parse(chunks));
+      });
+      request.on('end', function(){
+        sendEnd(response, 201, "ok", headers);
+      });
+   } else if (request.url === '/classes/room1') {
+      request.on('data', function(chunks) {
+        messages.push(JSON.parse(chunks));
+      });
+      request.on('end', function(){
+        sendEnd(response, 201, "ok", headers);
+      });
+   }
   }
+}
 
+var sendEnd = function(response, statusCode, responseData, headers) {
+  response.writeHead(statusCode, headers);
+  response.end(responseData);
 };
+
+exports.requestHandler = requestHandler;
+// var headers = {
+//   "access-control-allow-origin": "*",
+//   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+//   "access-control-allow-headers": 'Content-Type, Authorization, Content-Length, X-Requested-With',
+//   "access-control-max-age": 10, // Seconds.
+//   "Content-Type": "application/json"
+// };
+
+// var objectId = 1;
+
+// var collectData = function (response, callback) {
+//     var fullBody = '';
+//     request.on('data', function (chunk) {
+//       fullBody += chunk;
+//     });
+//     request.on('end',  function () {
+//       callback(JSON.parse(fullBody));
+//     });
+//   }
+
+// exports.send = function(response, data, statusCode) {
+//   statusCode = statusCode || 200
+//   response.writeHead(statusCode, headers);
+//   response.end(JSON.stringify(data));
+// }
+
+// var messages = [
+//  {
+//   text: 'sup',
+//   username: 'fred',
+//   objectId: objectId
+//  }
+// ]; //results: messages
+
+// module.exports = function(request, response) {
+
+//   var responseData = null;
+//   var statusCode = null;
+//   var fullBody = '';
+//   var hasRequestMethod = false;
+
+//   if (request.method === "GET") {
+
+//     exports.send(response, {results: messages});
+
+//   } else if (request.method === "POST") {
+
+//     collectData(response, function(message) {
+//       message.objectId = ++objectId;
+//       messages.push(message);
+//       exports.send(response, {objectId: objectId}, 201)
+//     });
+
+//     console.log('messages after', messages);
+
+//   } else if (request.method === "OPTIONS") {
+//       exports.send(response, null);
+//   } else {
+//     exports.send(response, '', 404);
+//   }
+
+// };
